67859b92b186d37b58155bd2ce1c2ba1f064c028
kannan-fsp <fspkannan@gmail.com>
2021-04-07 16:22:25 +0530

    First comment on test branch

---
+++ src/Myfirst/HelloProgram.java
@@ -0,0 +1,10 @@
+package Myfirst;
+
+public class HelloProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the sample project to run this program in git!");
+	}
+
+}
